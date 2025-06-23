e6f192e66a200209a2dafbf5c93847c254d1442f
Thibaud <dolomitt@users.noreply.github.com>
2025-06-23 13:07:42 +0800

    updating config

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,14 +1,14 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-console.log('VITE_ALLOWED_HOSTS:', process.env.VITE_ALLOWED_HOSTS);
+console.log('VITE_ALLOWED_HOSTS:', import.meta.env.VITE_ALLOWED_HOSTS);
 
 export default defineConfig({
   plugins: [react()],
   server: {
     port: 5173,
     host: '0.0.0.0',
-    allowedHosts: [process.env.VITE_ALLOWED_HOSTS],
+    allowedHosts: [import.meta.env.VITE_ALLOWED_HOSTS],
     proxy: {
       '/api': 'http://localhost:5000',
     },
